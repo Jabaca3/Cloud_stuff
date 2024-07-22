8eb79dfbc360b2b9c8b9735fa0598c658cc7289f
Joseph Baca <jbaca@pop-os.localdomain>
2024-07-22 15:46:00 -0600

    video service awaites request

--- video-processing/src/index.ts
+++ video-processing/src/index.ts
@@ -2,12 +2,28 @@ import express from "express";
 import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.post("/process-video", (req, res) => {
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad Request: Missing file path.");
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+      res.status(200).send("Process completed successfully ");
+    })
+    .on("error", (err) => {
+      console.log(`An error occured: ${err} `);
+      res.status(500).send(`internal service error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Video processing listening at http://localhost:${port}`);
 });
